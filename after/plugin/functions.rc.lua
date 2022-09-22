013de1612356f1abf342f233066130bc3120ce33
Jayden <yawzyn001@mymail.sim.edu.sg>
2022-09-23 05:05:55 +0630

    fix auto comment of next line

--- after/plugin/functions.rc.lua
+++ after/plugin/functions.rc.lua
@@ -2,4 +2,6 @@ vim.cmd([[
   if has("autocmd")
     au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
   endif
+
+  autocmd BufNewFile,BufRead * setlocal formatoptions-=ro
 ]])
--- lua/jayden/plugins.lua
+++ lua/jayden/plugins.lua
@@ -52,12 +52,7 @@ packer.startup(function(use)
 	use("lewis6991/gitsigns.nvim")
 	use("dinhhuy258/git.nvim") -- For git blame & browse
 
-	use({
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	})
+	use("numToStr/Comment.nvim")
 	use("kylechui/nvim-surround")
 	use("phaazon/hop.nvim")
 	use("napmn/react-extract.nvim")
