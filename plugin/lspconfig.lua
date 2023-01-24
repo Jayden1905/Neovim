ef248ab23aa18dc9aaecd5cda3d36cf9b26f9f09
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-01-24 17:17:28 +0800

    changing lspconfig

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -100,6 +100,11 @@ nvim_lsp.clangd.setup({
 	capabilities = capabilities,
 })
 
+nvim_lsp.jsonls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
 nvim_lsp.sumneko_lua.setup({
 	on_attach = on_attach,
 	settings = {
